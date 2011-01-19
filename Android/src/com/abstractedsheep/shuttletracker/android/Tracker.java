303ffb05a9bf8cf3849188f6317d20f4f5d3b0a8
Austin Wagner <austinwagner@gmail.com>
2011-01-19 09:13:30 -0500

    Added Jackson Apache Licensed (better than LGPL?) libraries.
    Signed-off-by: Austin Wagner <austinwagner@gmail.com>

--- Android/src/com/abstractedsheep/shuttletracker/android/Tracker.java
+++ Android/src/com/abstractedsheep/shuttletracker/android/Tracker.java
@@ -1,7 +1,16 @@
 package com.abstractedsheep.shuttletracker.android;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.codehaus.jackson.*;
+import org.codehaus.jackson.map.*;
+
+import com.abstractedsheep.shuttletracker.shared.Shuttle;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tracker extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +18,27 @@ public class Tracker extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ObjectMapper mapper = new ObjectMapper();
+        Shuttle s = new Shuttle(2, 0);
+        
+        java.io.ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+			mapper.writeValue(baos, s);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		TextView tv = (TextView)findViewById(R.id.hello);
+        String mapperOutput = new String(baos.toByteArray());
+        tv.setText(mapperOutput);
+        
     }
 }
--- Shared/src/com/abstractedsheep/shuttletracker/shared/Shuttle.java
+++ Shared/src/com/abstractedsheep/shuttletracker/shared/Shuttle.java
@@ -2,12 +2,25 @@ package com.abstractedsheep.shuttletracker.shared;
 
 public class Shuttle {
 	private int shuttleId;
+	private int routeId;
 	
-	public Shuttle(int shuttleId) {
+	public Shuttle(int shuttleId, int routeId) {
 		this.shuttleId = shuttleId;
 	}
 	
 	public int getShuttleId() {
 		return this.shuttleId;
+	}
+	
+	public void setShuttleId(int shuttleId) {
+		this.shuttleId = shuttleId;
+	}
+	
+	public void setRouteId(int routeId) {
+		this.routeId = routeId;
+	}
+	
+	public int getRouteId() {
+		return routeId;
 	}
 }
