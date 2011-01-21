42fdea429c28ead10b3475615339652010285aea
Umesh Jonnalagadda <velschtein@gmail.com>
2011-01-21 11:17:33 -0500

    added Route.java class to shared project.
    Signed-off-by: Umesh Jonnalagadda <velschtein@gmail.com>

---
+++ Shared/src/com/abstractedsheep/shuttletracker/shared/Route.java
@@ -0,0 +1,19 @@
+package com.abstractedsheep.shuttletracker.shared;
+
+/**
+ * Sample output from JSONExtractor.java
+color #E1501B
+id 1
+name West Route
+width 4
+latitude 42.72276
+longitude -73.67982
+latitude 42.72326
+longitude -73.68052
+ * @author jonnau
+ *
+ */
+
+public class Route {
+
+}
