6dba446534b3f99a92d173cd084d50ce7831de89
Brendon Justin <SNES350@gmail.com>
2011-01-29 17:09:25 -0500

    Create and add a new iOS XCode project.

---
+++ iOS/iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  Shuttle-Tracker
+//
+//  Created by Brendon Justin on 1/29/11.
+//  Copyright 2011 Naga Softworks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
