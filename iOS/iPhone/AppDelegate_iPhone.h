305f3a6d8f6130286a03ee4b47a712bff79a1143
Brendon Justin <SNES350@gmail.com>
2011-01-28 23:23:11 -0500

    Mixed commit, bad idea.

---
+++ iOS/iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  iPhone
+//
+//  Created by Brendon Justin on 1/27/11.
+//  Copyright 2011 Naga Softworks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
