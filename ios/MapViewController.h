701d06902ded814929d05328ebe690d80f6a66bf
Brendon Justin <SNES350@gmail.com>
2011-02-06 22:40:17 -0500

    Merge iOS version back into main repository.

---
+++ ios/MapViewController.h
@@ -0,0 +1,24 @@
+//
+//  MapViewController.h
+//  Shuttle-Tracker
+//
+//  Created by Brendon Justin on 1/29/11.
+//  Copyright 2011 Brendon Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+
+@class KMLParser;
+
+@interface MapViewController : UIViewController <MKMapViewDelegate> {
+	MKMapView *mapView;
+    KMLParser *routeKmlParser;
+    
+    NSArray *routes;
+    NSArray *stops;
+    NSMutableArray *vehicles;
+}
+
+@end
