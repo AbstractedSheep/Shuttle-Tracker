cdcf73c517e72b3f7c7175d1f4422b14f665be4e
Umesh Jonnalagadda <velschtein@gmail.com>
2011-02-08 17:08:22 -0500

    weird merge resolutions and also added a new library to the server project. The classpath includes this.
    Signed-off-by: Umesh Jonnalagadda <velschtein@gmail.com>

--- server/src/com/abstractedsheep/ShuttleTrackerServer/JSONSender.java
+++ server/src/com/abstractedsheep/ShuttleTrackerServer/JSONSender.java
@@ -5,7 +5,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -37,12 +37,12 @@ public class JSONSender {
 			gen.writeStartObject();
 			for(Shuttle shuttle : shuttleList) {
 				gen.writeObjectFieldStart(shuttle.getName());
-				gen.writeNumberField("Longitude", shuttle.getCurrentLocation().getLon());
-				gen.writeNumberField("Latitude", shuttle.getCurrentLocation().getLat());
+				gen.writeNumberField("Longitude", Shuttle.getCurrentLocation().getLon());
+				gen.writeNumberField("Latitude", Shuttle.getCurrentLocation().getLat());
 				gen.writeArrayFieldStart("ETA");
 				map = shuttle.getStopETA();
 				for(String stop : map.keySet()) {
-					gen.writeString(stop + " " + map.get((stop)) + " " + shuttle.getStops().get(stop).getLocation());
+					gen.writeString(stop + " " + getTimeStamp(map.get(stop)));
 				}
 				
 				gen.writeEndArray();
@@ -54,5 +54,43 @@ public class JSONSender {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	/**
+	 * save this data to the database
+	 * @param shuttleList
+	 */
+	public static void saveToDatabase(HashSet<Shuttle> shuttleList) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		Connection connection = null;
+		try {
+			Class.forName(driver);
+			String serverName = "127.0.0.1";
+			String dbName = "shuttle_tracker";
+			
+			String url = "jdbc:mysql://" + serverName +  "/" + dbName;
+			String usr = "";
+			String pass = "";
+			connection = DriverManager.getConnection(url, usr, pass);
+			
+			Statement stmt = connection.createStatement();
+			for(Shuttle shuttle : shuttleList) {
+				for(String stop : shuttle.getStopETA().keySet()){
+					String sql = "UPDATE shuttle_eta SET eta = " + shuttle.getStopETA().get(stop) +
+								 "WHERE shuttle_id = " + shuttle.getShuttleId() + "AND stop_id = " +
+								 shuttle.getStops().get(stop).getShortName();
+					int updateCount = stmt.executeUpdate(sql);
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private static String getTimeStamp(Integer integer) {
+		String str = new Timestamp(System.currentTimeMillis() + integer).toString();
+		return str.substring(0, str.indexOf('.'));
 	}
 }
