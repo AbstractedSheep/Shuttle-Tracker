8d8235cda47fc58c1e200ed1e64142fb32b974f6
Umesh Jonnalagadda <velschtein@gmail.com>
2011-02-12 09:40:18 -0500

    cleaned up the code a little bit.
    Signed-off-by: Umesh Jonnalagadda <velschtein@gmail.com>

--- server/src/com/abstractedsheep/ShuttleTrackerServer/JSONSender.java
+++ server/src/com/abstractedsheep/ShuttleTrackerServer/JSONSender.java
@@ -11,40 +11,48 @@ import java.util.HashSet;
 import com.abstractedsheep.extractor.Shuttle;
 
 /**
- * Connects to http://www.abstractedsheep.com/phpMyAdmin/ and writes data to the DB
+ * Connects to http://www.abstractedsheep.com/phpMyAdmin/ and writes data to the
+ * DB
+ * 
  * @author jonnau
- *
+ * 
  */
 public class JSONSender {
-	
+
 	/**
 	 * save this data to the database
+	 * 
 	 * @param shuttleList
 	 */
 	public static void saveToDatabase(HashSet<Shuttle> shuttleList) {
 		String driver = "com.mysql.jdbc.Driver";
 		Connection connection = null;
 		String[] args = null;
-		//System.out.println((new File("sts.properties").getAbsolutePath()));
+		// System.out.println((new File("sts.properties").getAbsolutePath()));
 		try {
 			Class.forName(driver).newInstance();
 			args = getArgumentsFromPropertiesFile("sts.properties");
 			connection = DriverManager.getConnection(args[0], args[1], args[2]);
 			System.out.println("Connected to database");
 			Statement stmt = connection.createStatement();
-			for(Shuttle shuttle : shuttleList) {
-				for(String stop : shuttle.getStopETA().keySet()){
-					String sql = "UPDATE shuttle_eta SET eta = '" + getTimeStamp(shuttle.getStopETA().get(stop)) +
-								 "' WHERE shuttle_id = " + shuttle.getShuttleId() + " AND stop_id = '" +
-								 shuttle.getStops().get(stop).getShortName() + "'";
+			for (Shuttle shuttle : shuttleList) {
+				for (String stop : shuttle.getStopETA().keySet()) {
+					String sql = "UPDATE shuttle_eta SET eta = '"
+							+ getTimeStamp(shuttle.getStopETA().get(stop))
+							+ "' WHERE shuttle_id = " + shuttle.getShuttleId()
+							+ " AND stop_id = '"
+							+ shuttle.getStops().get(stop).getShortName() + "'";
 					System.out.println(sql);
 					int updateCount = stmt.executeUpdate(sql);
-					
-					if(updateCount == 0) {
+
+					if (updateCount == 0) {
 						String insertHeader = "INSERT INTO shuttle_eta (shuttle_id, stop_id, eta) ";
-						String interValues = "VALUES (" + shuttle.getShuttleId() + ",'"
-									+ shuttle.getStops().get(stop).getShortName() + "','" +
-									getTimeStamp(shuttle.getStopETA().get(stop)) + "')";
+						String interValues = "VALUES ("
+								+ shuttle.getShuttleId() + ",'"
+								+ shuttle.getStops().get(stop).getShortName()
+								+ "','"
+								+ getTimeStamp(shuttle.getStopETA().get(stop))
+								+ "')";
 						stmt.executeUpdate(insertHeader + interValues);
 					}
 				}
@@ -53,28 +61,33 @@ public class JSONSender {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			try{
-				if(connection != null)
+			try {
+				if (connection != null)
 					connection.close();
-			} catch(SQLException e) {}
+			} catch (SQLException e) {
+			}
 		}
 	}
-	private static String[] getArgumentsFromPropertiesFile(String path) throws IOException {
+
+	private static String[] getArgumentsFromPropertiesFile(String path)
+			throws IOException {
 		String[] values = new String[3];
 		File f = new File(path);
 		System.out.println(f.getAbsolutePath());
 		BufferedReader buf = new BufferedReader(new FileReader(f));
 		String line = "";
 		line = buf.readLine();
-		for(int i = 0; (line != null); i++) {
+		for (int i = 0; (line != null); i++) {
 			values[i] = line;
 			line = buf.readLine();
 		}
 		buf.close();
 		return values;
 	}
+
 	private static String getTimeStamp(Integer integer) {
-		String str = new Timestamp(System.currentTimeMillis() + integer).toString();
+		String str = new Timestamp(System.currentTimeMillis() + integer)
+				.toString();
 		return str.substring(0, str.indexOf('.'));
 	}
 }
