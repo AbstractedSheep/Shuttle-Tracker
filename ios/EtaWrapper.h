cbfea6ac699bcc6731da7a5d438cdae33e3b436b
Brendon Justin <SNES350@gmail.com>
2011-02-20 15:23:22 -0500

    Recreate EtaWrapper class

---
+++ ios/EtaWrapper.h
@@ -0,0 +1,17 @@
+//
+//  EtaWrapper.h
+//  Shuttle-Tracker
+//
+//  Created by Brendon Justin on 2/20/11.
+//  Copyright 2011 Brendon Justin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface EtaWrapper : NSObject {
+    NSString *shuttleId;
+    NSString *stopId;
+}
+
+@end
