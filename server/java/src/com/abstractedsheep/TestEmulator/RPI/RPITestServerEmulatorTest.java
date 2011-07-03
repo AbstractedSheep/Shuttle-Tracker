d950e795e61b0ba6fa7f05bb9a3ab4984d2b5f4f
Umesh Sai Jonnalagadda <velschtein@gmail.com>
2011-07-03 10:43:02 -0400

    Test class to simulate RPI shuttle route..Useful if the shuttles are currently not running.
    Signed-off-by: Umesh Sai Jonnalagadda <velschtein@gmail.com>

---
+++ server/java/src/com/abstractedsheep/TestEmulator/RPI/RPITestServerEmulatorTest.java
@@ -0,0 +1,13 @@
+package com.abstractedsheep.TestEmulator.RPI;
+
+public class RPITestServerEmulatorTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
