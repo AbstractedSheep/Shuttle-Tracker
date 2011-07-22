6d9c3bcefeb6b77de72d54b96ccf894b7f0abd73
Umesh Jonnalagadda <velschtein@gmail.com>
2011-07-22 16:54:50 -0400

    Completed wrapper class for logging

--- server/java/src/com/abstractedsheep/Logger/Logger.java
+++ server/java/src/com/abstractedsheep/Logger/Logger.java
@@ -1,5 +1,9 @@
 package com.abstractedsheep.Logger;
 
+import com.abstractedsheep.config.STSProperties;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
 /**
  * Created by IntelliJ IDEA.
  * User: ujonnalagadda
@@ -7,5 +11,21 @@ package com.abstractedsheep.Logger;
  * Time: 4:02 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Logger {
+public class Logger extends java.util.logging.Logger{
+    protected Logger(String s, String s1) {
+        super(s, s1);
+    }
+
+    public static Logger getConfiguredLogger(Class c) {
+        Logger log = (Logger) getLogger(c.getCanonicalName());
+        String path = STSProperties.LOG_PATH + "/sts.log";
+
+        try {
+            log.addHandler(new FileHandler(path));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        return log;
+    }
 }
