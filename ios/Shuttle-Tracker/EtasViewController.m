f06f51044c45ad0810f65be5d10754cba484d2c9
Brendon Justin <brendonjustin@gmail.com>
2012-04-02 10:23:47 -0400

    Clear the points on a route before updating it from the JSON.
    Also improve cosmetics.

--- ios/Shuttle-Tracker/EtasViewController.m
+++ ios/Shuttle-Tracker/EtasViewController.m
@@ -520,7 +520,9 @@ const BOOL makeLaunchImage = NO;
                         /*
                          Replace this implementation with code to handle the error appropriately.
                          
-                         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+                         abort() causes the application to generate a crash log and terminate. 
+                         You should not use this function in a shipping application, although 
+                         it may be useful during development. 
                          */
                         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
 
--- ios/Shuttle-Tracker/ExtraEtasViewController.m
+++ ios/Shuttle-Tracker/ExtraEtasViewController.m
@@ -261,7 +261,9 @@
                 /*
                  Replace this implementation with code to handle the error appropriately.
                  
-                 abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+                 abort() causes the application to generate a crash log and terminate. 
+                 You should not use this function in a shipping application, although 
+                 it may be useful during development. 
                  */
                 NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
             }
