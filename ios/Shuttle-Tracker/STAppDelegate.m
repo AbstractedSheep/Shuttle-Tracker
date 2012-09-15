de80dcd0309571853af6a15c712268807565b8ae
Brendon Justin <brendonjustin@gmail.com>
2012-09-15 17:03:11 -0500

    Reduce maximum line lengths in STAppDelegate.
    Also remove unused boilerplate functions.

--- ios/Shuttle-Tracker/STAppDelegate.m
+++ ios/Shuttle-Tracker/STAppDelegate.m
@@ -53,19 +53,25 @@
     self.timeDisplayFormatter = self.dataManager.timeDisplayFormatter;
     
     STMapViewController *mapViewController = [[STMapViewController alloc] init];
-    mapViewController.tabBarItem = [[[UITabBarItem alloc] initWithTitle:@"Map" image:[UIImage imageNamed:@"glyphish_map"] tag:0] autorelease];
+    mapViewController.tabBarItem = [[[UITabBarItem alloc] initWithTitle:@"Map"
+                                                                  image:[UIImage imageNamed:@"glyphish_map"]
+                                                                    tag:0] autorelease];
     mapViewController.dataManager = self.dataManager;
     mapViewController.managedObjectContext = self.managedObjectContext;
     
-    UINavigationController *mapNavController = [[UINavigationController alloc] initWithRootViewController:mapViewController];
+    UINavigationController *mapNavController = [[UINavigationController alloc]
+                                                initWithRootViewController:mapViewController];
     [mapViewController release];
     
     STEtasViewController *etasViewController = [[STEtasViewController alloc] init];
-    etasViewController.tabBarItem = [[[UITabBarItem alloc] initWithTitle:@"ETAs" image:[UIImage imageNamed:@"glyphish_clock"] tag:1] autorelease];
+    etasViewController.tabBarItem = [[[UITabBarItem alloc] initWithTitle:@"ETAs"
+                                                                   image:[UIImage imageNamed:@"glyphish_clock"]
+                                                                     tag:1] autorelease];
     etasViewController.dataManager = self.dataManager;
     etasViewController.managedObjectContext = self.managedObjectContext;
     
-    UINavigationController *etasTableNavController = [[UINavigationController alloc] initWithRootViewController:etasViewController];
+    UINavigationController *etasTableNavController = [[UINavigationController alloc]
+                                                      initWithRootViewController:etasViewController];
     [etasViewController release];
     
     //  Note that this class (MainViewController) gets a reference to timeDisplayFormatter
@@ -139,36 +145,6 @@
     
     [self.window makeKeyAndVisible];
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
